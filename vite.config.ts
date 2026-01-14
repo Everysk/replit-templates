0179f66a47893c11796af1399759f86a8f6bb7e2
faguiarr <faguiar@everysk.com>
2026-01-14 19:55:16 +0000

    feat(COD-14296): add devcontainer files

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig, loadEnv } from "vite";
+
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+import { createDevServerProxy } from "./vite/serverProxy";
+import { envVarsLocationPlugin } from "./vite/plugins/envVarsLocation";
+import { serveDevAppConfigPlugin } from "./vite/plugins/serveDevAppConfig";
+
+export default defineConfig(({ mode, command }) => {
+  const root = process.env.PROJECT_ROOT ?? process.cwd();
+
+  const env = loadEnv(mode, root, "");
+  Object.assign(process.env, env);
+
+  return {
+    base: "./",
+    envDir: root,
+
+    build: {
+      outDir: "./dist",
+      emptyOutDir: true,
+    },
+
+    server: {
+      host: true,
+      port: Number(process.env.PORT) || 5173,
+      strictPort: true,
+      proxy: command === "serve" ? createDevServerProxy() : undefined,
+    },
+
+    plugins: [
+      react({
+        babel: {
+          plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+      tailwindcss(),
+      envVarsLocationPlugin(),
+      serveDevAppConfigPlugin(),
+    ],
+  };
+});
+ 
